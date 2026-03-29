6cc7f2e8d4ee2e13fd561903947a97ef60606144
seeni23 <svmaster141@gmail.com>
2026-03-29 15:04:26 +0530

    fix build file

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 python -m pip install --upgrade pip
+pip install fastapi uvicorn
 pip install -r backend/requirements.txt
-playwright install --with-deps chromium
+playwright install chromium
